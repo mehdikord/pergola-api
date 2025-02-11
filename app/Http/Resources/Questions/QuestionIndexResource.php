9707438d8ae34883b0b628cb6e6912a302cda0a3
mehdikord <mehdi.kord171@gmail.com>
2025-02-12 01:28:24 +0330

    upgrade

--- app/Http/Resources/Questions/QuestionIndexResource.php
+++ app/Http/Resources/Questions/QuestionIndexResource.php
@@ -3,6 +3,8 @@
 namespace App\Http\Resources\Questions;
 
 use App\Http\Resources\Colors\ColorShortResource;
+use App\Http\Resources\Options\OptionShortResource;
+use App\Models\Option;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -22,11 +24,27 @@ class QuestionIndexResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $items = [];
+        if ($this->items){
+            $get_items = json_decode($this->items, false, 512, JSON_THROW_ON_ERROR);
+            foreach ($get_items as $item){
+                $option = Option::find($item->id);
+                if ($option){
+                    $items[]=[
+                        'option' => new OptionShortResource($option),
+                        'value' => $item->value,
+                    ];
+                }
+            }
+
+        }
+
+
         return [
             'id' => $this->id,
             'from_color_id' => $this->from_color_id,
             'to_color_id' => $this->to_color_id,
-            'items' => json_decode($this->items, false, 512, JSON_THROW_ON_ERROR),
+            'items' => $items,
             'from_color' => new ColorShortResource($this->from_color),
             'to_color' => new ColorShortResource($this->to_color),
             'answers' => QuestionAnswerIndexResource::collection($this->answers),
